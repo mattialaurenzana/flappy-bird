f9b44923959aeeff5492f6d5e595e9d89b2a605c
marcoottaiano <marco.ottaiano00@gmail.com>
2022-09-20 17:20:49 +0200

    bug fix

--- src/screens/game/Game.js
+++ src/screens/game/Game.js
@@ -1,33 +1,44 @@
 import { useEffect, useState } from "react";
 import './game.css'
+import eventsBus from '../../events/eventsBus'
 
 function Game() {
     const [state, setState] = useState({
         username: '',
-        score: 0
+        score: 0,
+        shuttleClass: '',
+        shuttleContainerClass:''
     })
     let actualScore = 0
 
 
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            actualScore = actualScore + 1
-            setState({
-                ...state,
-                score: actualScore
-            })
-        }, 1000);
-        return () => clearInterval(interval);
-    }, []);
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         actualScore = actualScore + 1
+    //         setState({
+    //             ...state,
+    //             score: actualScore
+    //         })
+    //     }, 1000);
+    //     return () => clearInterval(interval);
+    // }, []);
+
+    function jumpShuttle(){
+        setState({
+            ...state,
+            shuttleContainerClass: 'jump',
+            shuttleClass: 'jump-shuttle'
+        })
+    }
 
 
     return (
         <>
-            <div className="game-container">
+            <div className="game-container" onClick={jumpShuttle}>
                 <div className="score-text">Score: {state.score}</div>
-                <div className="shuttle-container">
-                    <img className="shuttle pixelart" src={require('../../assets/image/sprite-sheet.png')}></img>
+                <div className={`shuttle-container ${state.shuttleContainerClass}`}>
+                    <img className={`shuttle pixelart ${state.shuttleClass}`} src={require('../../assets/image/sprite-sheet.png')}></img>
                 </div>
 
             </div>
