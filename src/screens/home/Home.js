7e3a69242801bc73c37e047f1172a38172000df7
marcoottaiano <marco.ottaiano00@gmail.com>
2022-09-20 10:46:33 +0200

    Adding Home

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,5 +1,9 @@
+import { SpriteAnimator } from 'react-sprite-animator';
 import BgContainer from '../../components/ui/bgcontainer/BgContainer';
 import './Home.css'
+import Button from '../../components/ui/button/Button';
+import InputBox from '../../components/ui/inputbox/InputBox';
+
 
 function Home() {
 
@@ -8,9 +12,27 @@ function Home() {
             <BgContainer />
             <div className='home-container'>
                 <h1 className='title'>Flappy Ufo</h1>
-            <div className='img-container'></div>
+                <div className='img-container'>
+                    {/* <SpriteAnimator
+                        sprite={'./heart.svg'}
+                        width={36}
+                        height={36}
+                        shouldAnimate={true}
+                        startFrame={0}
+                        stopLastFrame={true}
+                    /> */}
+                </div>
+                <InputBox
+                placeholder={'Inserire username...'}
+                />
+                <Button
+                label={'Start'}
+                />
+                <Button
+                label={'Classifica'}
+                />
             </div>
-            
+
         </>
     );
 
